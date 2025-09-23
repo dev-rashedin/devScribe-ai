7d5f13871a42fa594bd35ea0486106d7c2730dfb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-23 19:15:23 +0600

    logo updated in login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router";
+
 const Login = () => {
   return (
     <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
       <div className='max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
         <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
-          <div>
-            <img
-              src='https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png'
-              className='w-32 mx-auto'
-              alt='Logo'
-            />
-          </div>
+            <Link
+              to='/'
+              className='flex gap-2 items-center cursor-pointer font-logo text-brand w-60'
+            >
+              <img src='/logo.png' alt='logo' className='w-10 h-10 -mt-1' />
+              <h1 className='text-2xl font-bold mt-1'>DevScribe AI</h1>
+            </Link>
+
           <div className='mt-12 flex flex-col items-center'>
             <h1 className='text-2xl xl:text-3xl font-extrabold'>Sign up</h1>
             <div className='w-full flex-1 mt-8'>
