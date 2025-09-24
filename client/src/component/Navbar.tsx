808c467274f6aed7656cf5788429a03c6fd98d57
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-24 22:56:28 +0600

    small screen them switcher visiblity issue fixed

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLinks, MobileMenu, ThemeSwitcher, Blob, Logo } from './ui';
 
 const Navbar = () => {
   return (
-    <main className='bg-navbar sticky top-0 w-full h-20 flex-center bg-navbar/10 z-50'>
+    <main className='bg-navbar sticky top-0 w-full h-16 lg:h-20 flex-center bg-navbar/10 z-50'>
       <nav className='flex-between boundary'>
         <Blob />
 
@@ -11,8 +11,10 @@ const Navbar = () => {
         <NavLinks />
 
         {/* theme switcher */}
-        <div className='hidden lg:block mt-[6px] border rounded-full h-8 w-8 flex-center hover:bg-hover transition'>
-          <ThemeSwitcher />
+        <div className='hidden lg:block '>
+          <span className='mt-[6px] lg:border rounded-full h-8 w-8 flex-center hover:bg-hover transition'>
+            <ThemeSwitcher />
+          </span>
         </div>
 
         {/* mobile menu */}
