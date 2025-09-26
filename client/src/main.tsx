8cb45088eb7c009f07a4baa8175d35075f743117
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-26 13:08:05 +0600

    router separated from main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,35 +7,10 @@ import { RouterProvider } from 'react-router';
 import { HelmetProvider } from 'react-helmet-async';
 import  ThemeProvider  from './providers/ThemeProvider';
 import AuthProvider from './providers/AuthProvider';
-import Home from './pages/Home';
-import CodeExplainForm from './component/forms/CodeExplainForm';
-import Summarizer from './pages/Summarizer';
-import ErrorPage from './pages/ErrorPage';
-import Login from './pages/Login';
-import Subscription from './pages/Subscription';
-import CodeRefactorForm from './component/forms/CodeRefactorForm';
-import ArticleGeneratorForm from './component/forms/ArticleGeneratorForm';
+import router from './routes/router';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage/>,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'subscription', element: <Subscription /> },
-      { path: 'signin', element: <Login /> },
-      { path: 'signup', element: <Login /> },
-      {
-        path: 'services', children: [
-          { path: 'code-explainer', element: <CodeExplainForm/> },
-          { path: 'doc-summarizer', element: <Summarizer /> },
-          { path: 'code-refactor', element: <CodeRefactorForm /> },
-          {path: 'article-generator', element: <ArticleGeneratorForm/>},
-      ]},
-    ],
-  },
-]);
+
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
---
+++ client/src/routes/router.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import CodeExplainForm from '../component/forms/CodeExplainForm';
+import Summarizer from '../pages/Summarizer';
+import ErrorPage from '../pages/ErrorPage';
+import Login from '../pages/Login';
+import Subscription from '../pages/Subscription';
+import CodeRefactorForm from '../component/forms/CodeRefactorForm';
+import ArticleGeneratorForm from '../component/forms/ArticleGeneratorForm';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage/>,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'subscription', element: <Subscription /> },
+      { path: 'signin', element: <Login /> },
+      { path: 'signup', element: <Login /> },
+      {
+        path: 'services', children: [
+          { path: 'code-explainer', element: <CodeExplainForm/> },
+          { path: 'doc-summarizer', element: <Summarizer /> },
+          { path: 'code-refactor', element: <CodeRefactorForm /> },
+          {path: 'article-generator', element: <ArticleGeneratorForm/>},
+      ]},
+    ],
+  },
+]);
+
+
+export default router
