ed224f9dfc5750be8acb923b7263d8168a1cfca1
Rashedin-063 <rashedinislam.06@gmail.com>
2025-10-01 14:13:46 +0600

    prompts display made conditional

--- client/src/component/AIOutput.tsx
+++ client/src/component/AIOutput.tsx
@@ -3,15 +3,35 @@ import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import 'highlight.js/styles/obsidian.css';
 import { CopyButton } from './ui';
+import { useState } from 'react';
+import { FaEye, FaEyeSlash } from '../data/icons';
 
 const AIOutput = ({ title, explanation }: TExplanation) => {
+  const [showPrompt, setShowPrompt] = useState(true);
   return (
     <main>
-      <div className=''>
-        <h3 className='text-lg font-semibold'>Prompt</h3>
-        <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
-          {title}
-        </Markdown>
+      <div className=' mb-6'>
+        <div className='flex justify-between items-center'>
+          <h3 className='text-lg font-semibold'>Prompt: </h3>
+          <button
+            onClick={() => setShowPrompt((prev) => !prev)}
+            className='hover:underline'
+          >
+            {showPrompt ? <span className='flex-center gap-2'>Hide <FaEyeSlash /></span> : <span className='flex-center gap-2'>
+              Show <FaEye /></span>}
+          </button>
+        </div>
+
+        {showPrompt && (
+          <div className='mt-2 p-4 rounded-lg bg-navbar border prose prose-sm prose-invert'>
+            <Markdown
+              remarkPlugins={[remarkGfm]}
+              rehypePlugins={[rehypeHighlight]}
+            >
+              {title}
+            </Markdown>
+          </div>
+        )}
       </div>
       <div className='w-full max-w-4xl mt-6  p-6 rounded-2xl  prose prose-lg dark:prose-invert leading-loose tracking-wide relative'>
         <CopyButton explanation={explanation} />
